417fa1e92e2c702d7c9e11351a2bce7655c0c7d4
xushen <hanxushen1105@sina.com>
2017-05-17 13:50:23 +0800

    fix bugs of last version(selkeyframes)

--- utils/SelKeyFrames.cpp
+++ utils/SelKeyFrames.cpp
@@ -5,12 +5,12 @@ int main(int argc, char * argv[])
 {
     if(argc != 3)
     {
-        cerr << "Usage: selKeyFrames InputDir OutputDir";
+        cerr << "Usage: selKeyFrames InputDir OutputDir desc nps nocts factor";
         exit(-1);
     }
     string inputDir = argv[1], outputDir = argv[2];
     TestOpencvSelKeyFrames tosk;
 
     tosk.GetAllFiles(inputDir, outputDir);
-    tosk.CalcKeyFrames(6,3000,0.5,"surf");
+    tosk.CalcKeyFrames(atoi(argv[5]),atoi(argv[4]),atoi(argv[6]),argv[3]);
 }
